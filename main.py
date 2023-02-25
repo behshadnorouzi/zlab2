75ba30cfbdeb1fad5e8567c9a6f342249ecdf32c
behshad <behshad.azad@gmail.com>
2023-02-25 22:11:00 +0330

    cloning repo

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-test
+from fastapi import FastAPI
+from git import Repo
+
+app = FastAPI()
+Repo.clone_from('https://github.com/zlab-foss/backend-interview.git', 'C:/Users/Behshad/Desktop/zlab/download')
+
+@app.post("/")
+async def root():
+    return Repo
+
+
+
+
